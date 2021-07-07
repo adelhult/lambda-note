fe125fe57eed328513dd104684aa478b58a36ce6
Eli Adelhult <jxz2000@hotmail.se>
2021-07-07 10:33:56 +0200

    added a small unit test for the escape chars

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,13 @@ pub use parser::{parse_doc, Block, Inline, Metadata};
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn work_in_progress() {
-        let contents = std::fs::read_to_string("test.ln")
-            .expect("Something went wrong reading the file");
-        
-        let (blocks, metadata) = parser::parse_doc(&contents);
+        let contents =
+            std::fs::read_to_string("test.ln").expect("Something went wrong reading the file");
+
+        let (blocks, metadata) = parse_doc(&contents);
 
         println!("{:?}", metadata);
 
@@ -18,6 +19,11 @@ mod tests {
             println!("{}", block);
         }
     }
+
+    #[test]
+    fn escape_chars() {
+        // TODO: add a more complete test later.
+        let (blocks, _) = parse_doc("\\alpha\\beta\\Lambda\\lambda\\Upsilon\\up!");
+        assert_eq!(blocks[0].to_string().trim(), "αβΛλϒ↑!".to_string());
+    }
 }
-
-
