3a14256dc72461684936d898e5faf660a8011354
Eli Adelhult <jxz2000@hotmail.se>
2021-08-31 21:08:49 +0200

    Add a working code extension

--- lambda_note_lib/src/extensions/code.rs
+++ lambda_note_lib/src/extensions/code.rs
@@ -1,6 +1,12 @@
-use std::collections::HashMap;
 use crate::extensions::{Extension, ExtensionVariant};
 use crate::translator::{DocumentState, OutputFormat};
+use lazy_static::lazy_static;
+use syntect::easy::HighlightLines;
+use syntect::highlighting::ThemeSet;
+use syntect::html::{
+    highlighted_html_for_string, styled_line_to_highlighted_html, IncludeBackground,
+};
+use syntect::parsing::SyntaxSet;
 
 /// **Native extension**: hides the content from the final output
 #[derive(Clone)]
@@ -24,12 +30,12 @@ impl Extension for Code {
         args: Vec<String>,
         fmt: OutputFormat,
         variant: ExtensionVariant,
-        state: &mut DocumentState
+        state: &mut DocumentState,
     ) -> Option<String> {
         match fmt {
-            OutputFormat::Html => html(state, variant),
-            OutputFormat::Latex => latex(state, variant),
-            _ => panic!("Output format {:?} not supported by code extension", fmt)
+            OutputFormat::Html => html(args, variant),
+            OutputFormat::Latex => latex(args, variant, state),
+            _ => None,
         }
     }
 
@@ -46,11 +52,71 @@ impl Extension for Code {
     }
 }
 
-fn html(state: &mut DocumentState, variant: ExtensionVariant) -> Option<String> {
-    todo!()
+fn html(args: Vec<String>, variant: ExtensionVariant) -> Option<String> {
+    lazy_static! {
+        static ref PS: SyntaxSet = SyntaxSet::load_defaults_newlines();
+        static ref TS: ThemeSet = ThemeSet::load_defaults();
+    }
+
+    let theme = &TS.themes["InspiredGitHub"];
+
+    // get the syntax based on the given input
+    // otherwise fallback to using plain text
+    let syntax = if let Some(language) = args.get(1) {
+        match PS.find_syntax_by_token(language.trim()) {
+            Some(syntax) => syntax,
+            None => PS.find_syntax_plain_text(),
+        }
+    } else {
+        PS.find_syntax_plain_text()
+    };
+
+    let code = match args.get(0) {
+        Some(value) => value.to_string(),
+        None => "".into(),
+    };
+
+    match variant {
+        ExtensionVariant::Block => Some(highlighted_html_for_string(&code, &PS, syntax, theme)),
+        ExtensionVariant::Inline => {
+            let mut h = HighlightLines::new(syntax, theme);
+            let regions = h.highlight(&code, &PS);
+            Some(styled_line_to_highlighted_html(
+                &regions[..],
+                IncludeBackground::No,
+            ))
+        }
+    }
 }
 
+fn latex(
+    args: Vec<String>,
+    variant: ExtensionVariant,
+    state: &mut DocumentState,
+) -> Option<String> {
+    let code = match args.get(0) {
+        Some(value) => value.to_string(),
+        None => "".into(),
+    };
 
-fn latex(state: &mut DocumentState, variant: ExtensionVariant) -> Option<String>{
-    todo!()
+    let language = format!(
+        "{{{}}}",
+        match args.get(1) {
+            Some(language) => language,
+            None => "text",
+        }
+    );
+
+    state.import("\\usepackage{minted}");
+
+    Some(match variant {
+        ExtensionVariant::Block => format!(
+            "\\begin{{minted}}{lang}\n{content}\n\\end{{minted}}",
+            lang = language,
+            content = code
+        ),
+        ExtensionVariant::Inline => {
+            format!("\\mint{lang}|{content}|", lang = language, content = code)
+        }
+    })
 }
