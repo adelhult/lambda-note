5bcb91ae76bb9981387981892799cb257905cae9
Eli Adelhult <hello@eli.nu>
2021-09-03 08:06:00 +0200

    Improved CLI: pandoc and live-server integration

--- lambda_cli/src/main.rs
+++ lambda_cli/src/main.rs
@@ -1,38 +1,71 @@
-use lambda_note_lib::{DocumentState, Latex, Html};
+use lambda_note_lib::{DocumentState, Html, Latex, Translator};
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use std::env;
-use std::fs;
-use std::path::PathBuf;
+use std::process::{Command, Stdio};
 use std::sync::mpsc::channel;
-use std::time::Duration;
+use std::{env, fs, path::PathBuf, time::Duration};
+use tempfile::tempdir;
 
+/// A simple CLI program to test lambda note
+/// Usage: lambda <INPUT FILE> <OUTPUT FILE>
+/// If only given one arg, a file watcher and a live updating
+/// server is started to live preview a html document.
 fn main() {
-    println!("λnote - demo cli");
-
+    // collect cli args
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let input: PathBuf = args.get(0).expect("No input file was provided").into();
-    let output: PathBuf = args.get(1).expect("no output file was provided").into();
+    match args.len() {
+        1 => live_preview(args),
+        2 => single_run(args),
+        _ => {
+            println!("Usage: lambda <INPUT FILE> <OUTPUT FILE>");
+            println!("You can omit the output file to start a live html preview.");
+        }
+    }
+}
 
-    match output.extension() {
+
+/// Read the file once, transpile it to the correct output format and write to
+/// the given output file.
+fn single_run(args: Vec<String>) {
+    let input_file: PathBuf = args.get(0).expect("No input file was provided").into();
+    let output_file: PathBuf = args.get(1).expect("no output file was provided").into();
+
+    match output_file.extension() {
         None => {
             println!("You need to have a file extension for the output file");
             return;
         }
         Some(extension) => match extension.to_str() {
-            Some("tex") => latex(&input, &output),
-            Some("html") => html(&input, &output),
-            _ => {
-                println!("Only .html and .tex files are valid as output");
+            Some("tex") => translate(&input_file, &output_file, Latex),
+            Some("html") => translate(&input_file, &output_file, Html),
+            
+            // The program will try to resolve non native output formats
+            // by passing a tex file to pandoc.
+            Some(extension_str) => {
+                println!("No native support for .{} files", extension_str);
+                println!("Generating a .tex file and forwarding it to pandoc");
+
+                let mut latex_file: PathBuf =
+                    output_file.clone().file_stem().expect("No filename").into();
+
+                latex_file.set_extension("tex");
+
+                translate(&input_file, &latex_file, Latex);
+                pandoc(&latex_file, &output_file);
+            }
+
+            None => {
+                println!("You need to have a file extension for the output file");
                 return;
             }
         },
     }
 }
 
-fn latex(input_file: &PathBuf, output_file: &PathBuf) {
+/// Given a translator and input write to an output file.
+fn translate<T: Translator + 'static>(input_file: &PathBuf, output_file: &PathBuf, translator: T) {
     let content = fs::read_to_string(input_file).expect("Something went wrong reading the file");
-    let mut doc = DocumentState::new(Latex);
+    let mut doc = DocumentState::new(translator);
     let result = doc.translate(&content);
 
     fs::write(output_file, result).expect("Unable to write file");
@@ -44,30 +77,60 @@ fn latex(input_file: &PathBuf, output_file: &PathBuf) {
     );
 }
 
-fn html(input_file: &PathBuf, output_file: &PathBuf) {
+
+/// Invoke pandoc to translate between two formats
+fn pandoc(input_file: &PathBuf, output_file: &PathBuf) {
+    match Command::new("pandoc")
+        .arg(input_file)
+        .arg(format!("-o {}", output_file.to_string_lossy()))
+        .output()
+    {
+        Err(error) => {
+            println!("Failed to invoke pandoc. Error: {}", error);
+        }
+        Ok(_) => (),
+    }
+}
+
+/// Start a HTML live preview.
+/// Invokes the external node program "liveserver" and autoupdates  
+fn live_preview(args: Vec<String>) {
+    let input_file: PathBuf = args.get(0).expect("No input file was provided").into();
+
     println!("Starting a live updating version");
+
+    let dir = tempdir().expect("Failed to create a temp directory");
+    let output_file_path = dir.path().join("index.html");
 
     let (tx, rx) = channel();
     let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(1)).unwrap();
-    watcher.watch(input_file, RecursiveMode::Recursive).unwrap();
+
+    watcher
+        .watch(&input_file, RecursiveMode::Recursive)
+        .unwrap();
+
+    translate(&input_file, &PathBuf::from(&output_file_path), Html);
+
+    match Command::new("cmd")
+        .current_dir(dir.path())
+        .arg("/C")
+        .arg("live-server")
+        .arg("index.html")
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Err(error) => {
+            println!("Failed to start the live-server, error: {}", error);
+            return;
+        }
+        Ok(_) => (),
+    }
 
     loop {
         match rx.recv() {
             Ok(_) => {
                 println!("\n\n=== The file was rerendered ===");
-                let content =
-                    fs::read_to_string(input_file).expect("Something went wrong reading the file");
-
-                let mut doc = DocumentState::new(Html);
-                let result = doc.translate(&content);
-
-                fs::write(output_file, result).expect("Unable to write file");
-
-                println!(
-                    "errors:\n{}\nwarnings:{}",
-                    doc.errors.join("\n"),
-                    doc.warnings.join("\n")
-                );
+                translate(&input_file, &PathBuf::from(&output_file_path), Html);
             }
             _ => (),
         }
