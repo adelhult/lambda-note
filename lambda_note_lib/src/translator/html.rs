ec8eddc3eb5e2cc00362c733eb340a4ab056eaea
Eli Adelhult <hello@eli.nu>
2021-09-17 20:28:29 +0200

    new fonts and style for the html translator

--- lambda_note_lib/src/translator/html.rs
+++ lambda_note_lib/src/translator/html.rs
@@ -46,54 +46,62 @@ impl Translator for Html {
     ) -> String {
         format!(
             r#"
-    <!DOCTYPE html>
-    <html lang="{language}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        {imports}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet"> 
-            <style>
-            html, body, h1, h2, h3, h4, h5, h6 {{
-                font-family: 'Open Sans', sans-serif;
-            }}
-
-            .content {{
-                margin-top:2rem;
-                margin-bottom:4rem;
-                margin-left:auto;
-                margin-right:auto;
-                max-width:700px;
-            }}
-
-            img {{
-                max-width:100%;
-                text-align:center;
-            }}
-
-
-            pre {{
-                padding:0.8rem;
-                font-family: 'Fira Code', monospace;
-                box-sizing:border-box;
-                font-size:0.9rem;
-                border-radius:0.2rem;
-            }}
-        </style>
-        <title>{title}</title>
-    </head>
-    <body>
-        <div class="content">
-            {top}
-            {content}
-            {bottom}
-        </div>
-    </body>
-    </html>
-    "#,
+<!DOCTYPE html>
+<html lang="{language}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {imports}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <style>
+        *::-moz-selection, *::selection {{
+            background: #E2705B;
+            color:white;
+        }}
+        h1, h2, h3, h4, h5, h6 {{
+            font-family: 'Poppins', sans-serif;
+        }}
+        h1 {{
+            font-size: 1.7rem;
+        }}
+        h2 {{
+            font-size: 1.5rem;
+        }}
+        body, p {{
+            font-family: 'PT Serif', serif;
+        }}
+        .content {{
+            box-sizing: border-box;
+            padding:0.8rem;
+            margin-top:2rem;
+            margin-bottom:4rem;
+            margin-left:auto;
+            margin-right:auto;
+            max-width:750px;
+        }}
+        img {{
+            max-width:100%;
+        }}
+        pre {{
+            padding:0.8rem;
+            font-family: 'Fira Code', monospace;
+            box-sizing:border-box;
+            font-size:0.9rem;
+            border-radius:0.2rem;
+        }}
+    </style>
+    <title>{title}</title>
+</head>
+<body>
+    <div class="content">
+    {top}
+    {content}
+    {bottom}
+    </div>
+</body>
+</html>"#,
             imports = imports.iter().fold(String::new(), |acc, s| acc + s + "\n"),
             top = top,
             bottom = bottom,
