0772f0e807b6bb60cd87c3757f84ecfd4fb53ccb
Eli Adelhult <hello@eli.nu>
2022-01-04 18:43:03 +0100

    added smooth scrolling to html output

--- lambda_note_lib/src/translator/html.rs
+++ lambda_note_lib/src/translator/html.rs
@@ -64,6 +64,10 @@ impl Translator for Html {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:wght@700&display=swap" rel="stylesheet">
     <style>
+        html {{
+            scroll-behavior: smooth;
+        }}
+        
         *::-moz-selection, *::selection {{
             background: #E2705B;
             color:white;
