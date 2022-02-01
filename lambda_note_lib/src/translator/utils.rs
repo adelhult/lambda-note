f9b3f737e979fe29d3c8d84d0d116042d4a05d36
Eli Adelhult <hello@eli.nu>
2022-02-01 15:46:48 +0100

    removed utils

--- lambda_note_lib/src/translator/utils.rs
+++
@@ -1,7 +0,0 @@
-/// indents each line of the string by a given amount
-pub fn indent(text: &str, level: usize) -> String {
-    text
-        .lines()
-        .map(|line| format!("{:ident$}{msg}\n", "", ident=4*level, msg=line))
-        .collect()
-}
